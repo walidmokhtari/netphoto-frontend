0e4bec3e559d803f33259f1b89d0921922904d65
Walid <walid.mokhtari@etudiant.univ-perp.fr>
2022-02-21 11:06:17 +0100

    UPDATE 21-02-2022 11:06

--- src/components/navs/NavAdmin/NavAdmin.js
+++ src/components/navs/NavAdmin/NavAdmin.js
@@ -2,15 +2,31 @@ import React from 'react';
 import styles from './NavAdmin.module.scss'
 import {LocalMoviesRounded, CategoryRounded, HomeRounded } from '@material-ui/icons';
 import Link from 'next/link';
-
+import Router, { useRouter } from 'next/router';
 
 function NavAdmin(props) {
+
+    const router = useRouter();
+
+    const goToHome = () => {
+        router.push('/admin/dashbord');
+    }
+
+    const goToMovies = () => {
+        router.push('/admin/movies');
+    }
+
+    const goToCategories = () => {
+        router.push('/admin/categories');
+    }
+
+
     return (
         <nav className={styles.nav__list}>
             <ul>
-                <li><Link href="/admin/dashbord"><HomeRounded></HomeRounded><p>Accueil</p></Link></li>
-                <li><Link href="/admin/movies"><LocalMoviesRounded></LocalMoviesRounded><p>Gestion des films</p></Link></li>
-                <li><Link href="/admin/categories"><CategoryRounded></CategoryRounded><p>Gestion des catégories</p></Link></li>
+                <li onClick={goToHome}><HomeRounded></HomeRounded><p>Accueil</p></li>
+                <li onClick={goToMovies}><LocalMoviesRounded></LocalMoviesRounded><p>Gestion des films</p></li>
+                <li onClick={goToCategories}><CategoryRounded></CategoryRounded><p>Gestion des catégories</p></li>
             </ul>
         </nav>
     );
